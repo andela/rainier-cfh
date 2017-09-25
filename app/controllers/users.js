8b642e922421aef92b11bc55d0656b29ab336266
Enodi <enodiaudu5@gmail.com>
2017-09-25 16:27:56 +0100

    refactor(authentication): modify signup route

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -12,32 +12,27 @@ const User = mongoose.model('User');
 const avatars = require('./avatars').all();
 
 /**
-* Auth callback
-*/
-exports.authCallback = function(req, res, next) {
-res.redirect('/chooseavatars');
-};
-
-/**
 * Show login form
 */
-exports.signin = function(req, res) {
-if (!req.user) {
-  res.redirect('/#!/signin?error=invalid');
-} else {
-  res.redirect('/#!/app');
-}
+
+exports.signin = (req, res) => {
+  if (!req.user) {
+    res.redirect('/#!/signin?error=invalid');
+  } else {
+    res.redirect('/#!/app');
+  }
 };
 
 /**
 * Show sign up form
 */
-exports.signup = function(req, res) {
-if (!req.user) {
-  res.redirect('/#!/signup');
-} else {
-  res.redirect('/#!/app');
-}
+
+exports.signup = (req, res) => {
+  if (!req.user) {
+    res.redirect('/#!/signup');
+  } else {
+    res.redirect('/#!/app');
+  }
 };
 
 /**
@@ -52,32 +47,9 @@ exports.signout = (req, res) => {
 /**
 * Session
 */
-exports.session = function(req, res) {
-res.redirect('/');
-};
 
-/** 
-* Check avatar - Confirm if the user who logged in via passport
-* already has an avatar. If they don't have one, redirect them
-* to our Choose an Avatar page.
-*/
-exports.checkAvatar = function(req, res) {
-if (req.user && req.user._id) {
-  User.findOne({
-    _id: req.user._id
-  })
-  .exec(function(err, user) {
-    if (user.avatar !== undefined) {
-      res.redirect('/#!/');
-    } else {
-      res.redirect('/#!/choose-avatar');
-    }
-  });
-} else {
-  // If user doesn't even exist, redirect to /
+exports.session = (req, res) => {
   res.redirect('/');
-}
-
 };
 
 /**
@@ -156,82 +128,3 @@ exports.login = (req, res) => {
   });
 };
 
-/**
-* Assign avatar to user
-*/
-exports.avatars = function(req, res) {
-// Update the current user's profile to include the avatar choice they've made
-if (req.user && req.user._id && req.body.avatar !== undefined &&
-  /\d/.test(req.body.avatar) && avatars[req.body.avatar]) {
-  User.findOne({
-    _id: req.user._id
-  })
-  .exec(function(err, user) {
-    user.avatar = avatars[req.body.avatar];
-    user.save();
-  });
-}
-return res.redirect('/#!/app');
-};
-
-exports.addDonation = function(req, res) {
-if (req.body && req.user && req.user._id) {
-  // Verify that the object contains crowdrise data
-  if (req.body.amount && req.body.crowdrise_donation_id && req.body.donor_name) {
-    User.findOne({
-      _id: req.user._id
-    })
-    .exec(function(err, user) {
-      // Confirm that this object hasn't already been entered
-      var duplicate = false;
-      for (var i = 0; i < user.donations.length; i++ ) {
-        if (user.donations[i].crowdrise_donation_id === req.body.crowdrise_donation_id) {
-          duplicate = true;
-        }
-      }
-      if (!duplicate) {
-        console.log('Validated donation');
-        user.donations.push(req.body);
-        user.premium = 1;
-        user.save();
-      }
-    });
-  }
-}
-res.send();
-};
-
-/**
-*  Show profile
-*/
-exports.show = function(req, res) {
-var user = req.profile;
-
-res.render('users/show', {
-  title: user.name,
-  user: user
-});
-};
-
-/**
-* Send User
-*/
-exports.me = function(req, res) {
-res.jsonp(req.user || null);
-};
-
-/**
-* Find user by id
-*/
-exports.user = function(req, res, next, id) {
-User
-  .findOne({
-    _id: id
-  })
-  .exec(function(err, user) {
-    if (err) return next(err);
-    if (!user) return next(new Error('Failed to load User ' + id));
-    req.profile = user;
-    next();
-  });
-};

a97d6147d623779fbce2771cdd30c99bd6510523
Segun Ola <segunolalive@gmail.com>
2017-09-27 18:33:14 +0100

    chore(conflict): resolve conflict

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,100 @@ const nodemon = require('gulp-nodemon');
 const sass = require('gulp-sass');
 
 gulp.task('watch', () => {
-  gulp.watch('app/views/**', livereload());
-  gulp.watch('public/js/**', livereload());
-  gulp.watch('app/**/*.js', livereload());
-  gulp.watch('public/views/**', livereload());
-  gulp.watch('public/css/common.scss', ['sass']);
-  gulp.watch('public/css/**', livereload());
+  gulp.watch('app/views/**/*', ['jade']);
+  gulp.watch('public/js/**', ['public:js']);
+  gulp.watch('app/**/*.js', ['transpile-app']);
+  gulp.watch('public/views/**/*', ['html']);
+  gulp.watch('public/css/**/*.scss', ['sass']);
+  gulp.watch('public/css/**/*', ['css']);
 });
 
 
-gulp.task('nodemon', () => {
+gulp.task('lint', () => (
+  gulp.src([
+    'gulpfile.js',
+    'public/js/**/*.js',
+    'app/**/*.js',
+    'test/**/*.js'
+  ])
+    .pipe(eslint())
+    // .pipe(eslint.format())
+    // .pipe(eslint.failAfterError())
+));
+
+gulp.task('transpile-app', ['lint'], () => (
+  gulp.src('app/**/*.js')
+    .pipe(newer('dist/app'))
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/app'))
+));
+
+gulp.task('server', ['lint'], () => (
+  gulp.src('server.js')
+    .pipe(newer('dist'))
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      plugins: ['transform-runtime']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'))
+));
+
+gulp.task('jade', ['lint'], () => (
+  gulp.src('app/**/*.jade')
+    .pipe(newer('dist/app'))
+    .pipe(gulp.dest('dist/app'))
+));
+
+gulp.task('css', ['lint', 'sass'], () => (
+  gulp.src('public/**/*.css')
+    .pipe(newer('dist/public'))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/public'))
+));
+
+gulp.task('images', ['lint'], () => (
+  gulp.src('public/img/**/*')
+    .pipe(newer('dist/public/img'))
+    .pipe(imagemin({ optimizationLevel: 9 }))
+    .pipe(gulp.dest('dist/public/img'))
+));
+
+gulp.task('html', ['lint', 'images'], () => (
+  gulp.src('public/views/**/*.html')
+    .pipe(newer('dist/public/views/**/*.html'))
+    .pipe(htmlclean())
+    .pipe(gulp.dest('dist/public/views'))
+));
+
+gulp.task('public:js', ['lint'], () => (
+  gulp.src('public/js/**/*.js')
+    .pipe(newer('dist/public/js'))
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      plugins: ['transform-runtime']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/public/js'))
+));
+
+gulp.task('config:js', ['lint'], () => (
+  gulp.src('config/**/*.js')
+    .pipe(newer('dist/config'))
+    .pipe(babel())
+    .pipe(gulp.dest('dist/config'))
+));
+
+gulp.task('config:json', ['lint'], () => (
+  gulp.src('config/**/*.json')
+    .pipe(newer('dist/config'))
+    .pipe(gulp.dest('dist/config'))
+));
+
+gulp.task('nodemon', ['watch', 'build'], () => (
   nodemon({
     script: 'server.js',
     ext: 'js',
@@ -28,11 +112,11 @@ gulp.task('nodemon', () => {
   });
 });
 
-gulp.task('sass', () => {
-  gulp.src('public/css/common/scss')
+gulp.task('sass', () => (
+  gulp.src('public/css/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('public/css/'));
-});
+    .pipe(gulp.dest('dist/public/css/'))
+));
 
 gulp.task('lint', () => {
   gulp.src([

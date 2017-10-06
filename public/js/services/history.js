1efd630204751a993058358803f8a98996fab11d
dinobi <dino.kennetcorp@gmail.com>
2017-10-06 16:59:17 +0100

    chore(save-game): save game when game ends

--- public/js/services/history.js
+++ public/js/services/history.js
@@ -9,36 +9,31 @@
   * history services
   * Description: setup a history service
   */
-  .factory('history', ['$http',
-    ($http) => {
-      const history = {};
-      // method that saves a user game data
-      history.saveGameHistory = (route, gameSession) => {
-        return $http({
-          method: 'POST',
-          url: route,
-          headers: { 'Content-Type': 'application/json' },
-          data: {
-            gameOwner: gameSession.gameOwner,
-            gamePlayers: gameSession.gamePlayers
-          }
-        }).then((response) => {
-          return response.data;
-        });
+  .factory('history', ['$http', '$window',
+    ($http, $window) => {
+      // method that gets game history
+      const getGameLog = () => new Promise((resolve, reject) => {
+        $http.get('/api/games/history', { headers: { authorization: $window.localStorage.cfhToken } })
+          .success((response) => {
+            resolve(response);
+          })
+          .error((error) => {
+            reject(error);
+          });
+      });
+      const userDonations = () => new Promise((resolve, reject) => {
+        $http.get('/api/donations', { headers: { authorization: $window.localStorage.cfhToken } })
+          .success((response) => {
+            resolve(response);
+          })
+          .error((error) => {
+            reject(error);
+          });
+      });
+      return {
+        getGameLog,
+        userDonations
       };
-      // method that updates a user game data
-      history.updateGameHistory = (route, gameSession) =>
-        $http({
-          method: 'POST',
-          url: route,
-          headers: { 'Content-Type': 'application/json' },
-          data: {
-            gameSessionId: gameSession.gameKey,
-            gameWinner: gameSession.gameWinner,
-            status: gameSession.status,
-            gameRounds: gameSession.gameRounds
-          }
-        }).then(response => response.data);
 
       return history;
     }

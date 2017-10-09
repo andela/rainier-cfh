a359e9e592849629376d0a09e2d99090b13a6d0c
Enodi <enodiaudu5@gmail.com>
2017-10-09 10:50:02 +0100

    fix(social-authentication): resolve env variable value

--- config/env/development.js
+++ config/env/development.js
@@ -7,21 +7,21 @@ module.exports = {
   facebook: {
     clientID: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: `${process.env.APP_URL}/auth/facebook/callback`
   },
   twitter: {
     clientID: process.env.TWITTER_CLIENT_ID,
     clientSecret: process.env.TWITTER_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    callbackURL: `${process.env.APP_URL}/auth/twitter/callback`
   },
   github: {
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/github/callback'
+    callbackURL: `${process.env.APP_URL}/auth/github/callback`
   },
   google: {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: `${process.env.APP_URL}/auth/google/callback`
   }
 };
